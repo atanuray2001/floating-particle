cb574abd95166ef8f39bfbaa91bfb8f53e67d37f
Atanu Ray <atanuray2001@gmail.com>
2025-02-02 15:19:37 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,155 @@
+function loco(){
+    gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
+loco();
+
+
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  render();
+});
+
+function files(index) {
+  var data = `
+    ./img/111.jpg
+    ./img/1.jpg
+    ./img/2.jpg
+    ./img/222.jpg
+    ./img/3.jpg
+    ./img/4.jpg
+    ./img/333.jpg
+    ./img/5.jpg
+    ./img/6.jpg
+    ./img/444.jpg
+    ./img/7.jpg
+    ./img/8.jpg
+    ./img/555.jpg
+    ./img/9.jpg
+    ./img/10.jpg
+    ./img/666.jpg
+    ./img/11.jpg
+    ./img/12.jpg
+    ./img/777.jpg
+    ./img/13.jpg
+    ./img/14.jpg
+    ./img/888.jpg
+    ./img/15.jpg
+    ./img/16.jpg
+    ./img/999.jpg
+    ./img/17.jpg
+    ./img/18.jpg
+    ./img/1010.jpg
+    ./img/19.jpg
+    ./img/20.jpg
+    ./img/1111.jpg
+    ./img/22.jpg
+    ./img/23.jpg
+    ./img/1212.jpg
+    ./img/24.jpg
+    ./img/25.jpg
+    ./img/1313.jpg
+ `;
+  return data.split("\n")[index];
+}
+
+const frameCount = 37;
+
+const images = [];
+const imageSeq = {
+  frame: 1,
+};
+
+for (let i = 0; i < frameCount; i++) {
+  const img = new Image();
+  img.src = files(i);
+  images.push(img);
+}
+
+gsap.to(imageSeq, {
+  frame: frameCount - 1,
+  snap: "frame",
+  ease: `none`,
+  scrollTrigger: {
+    scrub: 0.15,
+    trigger: `#page>canvas`,
+    //   set start end according to preference
+    start: `top top`,
+    end: `300% top`,
+    scroller: `#main`,
+  },
+  onUpdate: render,
+});
+
+images[1].onload = render;
+
+function render() {
+  scaleImage(images[imageSeq.frame], context);
+}
+
+function scaleImage(img, ctx) {
+  var canvas = ctx.canvas;
+  var hRatio = canvas.width / img.width;
+  var vRatio = canvas.height / img.height;
+  var ratio = Math.max(hRatio, vRatio);
+  var centerShift_x = (canvas.width - img.width * ratio) / 2;
+  var centerShift_y = (canvas.height - img.height * ratio) / 2;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(
+    img,
+    0,
+    0,
+    img.width,
+    img.height,
+    centerShift_x,
+    centerShift_y,
+    img.width * ratio,
+    img.height * ratio
+  );
+}
+ScrollTrigger.create({
+
+  trigger: "#page>canvas",
+  pin: true,
+  // markers:true,
+  scroller: `#main`,
+//   set start end according to preference
+  start: `top top`,
+  end: `300% top`,
+});
+
